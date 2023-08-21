c2f0c9f683baa6791ad92e2299b832e447eb3e88
alizaintern <aliza.intern@devsinc.com>
2023-08-22 03:25:51 +0500

    Added Some new Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{useState} from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUpForm from './Components/SignUpForm'; // Update the path if needed
+import LoginForm from './Components/LoginForm'; // Update the path if needed
+import Feed from './Components/Feed';
+
+
+interface Record {
+  id:number,
+  username: string;
+ email: string;
+  password: string;
 }
 
+
+
+const App: React.FC = () => {
+  const [users, setUsers] = useState<Record[]>([]);
+ 
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUpForm  records={users} setRecords={setUsers}/>} />
+        <Route path="/LoginForm" element={<LoginForm records={users}/>} />
+        <Route path="/Feed" element={<Feed/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
 export default App;
+
+// import './App.css';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import SignUpForm from './Components/SignUpForm';
+// import LoginForm from './Components/LoginForm';
+
+
+// interface Record {
+//      username: string;
+//     email: string;
+//      password: string;
+//    }
+// const App: React.FC = () => {
+//   const [records, setRecords] = useState<Record[]>([]);
+
+//   const renderSignUpForm = () => (
+//     <SignUpForm records={records} setRecords={setRecords} />
+//   );
+
+//   const renderLoginForm = () => (
+//     <LoginForm records={records} />
+//   );
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//       <Route path="/SignUpForm" element={renderSignUpForm} />
+//       <Route path="/LoginForm" element={renderLoginForm} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
@@ -17,3 +18,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
